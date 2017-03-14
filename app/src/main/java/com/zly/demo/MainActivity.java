78786d696bcd14cd1b2beedc30f4fc85cdd68baf
zhuleiyue <zhuleiyue@gmail.com>
2017-03-14 18:52:35 +0800

    modify demo

--- app/src/main/java/com/zly/demo/MainActivity.java
+++ app/src/main/java/com/zly/demo/MainActivity.java
@@ -9,19 +9,5 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-//        CollapsedTextView textView = (CollapsedTextView) findViewById(R.id.tv);
-//        String str = "Hello World! Hello World! Hello World! Hello World! Hello World! Hello World! Hello World! Hello World! Hello World! Hello World! Hello World! Hello World! Hello World! Hello World! Hello World! Hello World! Hello World! Hello World! Hello World! Hello World! Hello World!";
-//        String str = "Hello World! Hello World! Hello World! Hello World! Hello World! Hello World! Hello World! Hello World! Hello World! Hello World! Hello World! Hello World! Hello World! Hello World! Hello World! Hello World! Hello World! Hello World! Hello World! Hello World!\nHello World!";
-//        textView.setText("Hello World! Hello World! Hello World! Hello World! Hello World! Hello World! Hello World! Hello World! Hello World! Hello World! Hello World! Hello World! Hello World! Hello World! Hello World! Hello World! Hello World! Hello World! Hello World! Hello World!");
-//        String str = "Hello World!\n\nHello World!\n\nHello World!\n\n\n\n\n";
-//        String str = "Hello World!\nHello World!\nHello World!\nHello World! Hello World! Hello World! Hello World! Hello World! Hello World! Hello World! Hello World! Hello World! Hello World! Hello World! Hello World! Hello World!";
-//        SpannableStringBuilder builder = new SpannableStringBuilder(str);
-//        Drawable drawable = ContextCompat.getDrawable(this, R.mipmap.ic_launcher);
-//        drawable.setBounds(0, 0, drawable.getIntrinsicWidth(), drawable.getIntrinsicHeight());
-//        builder.setSpan(new ImageSpan(drawable), 0, 2, Spanned.SPAN_INCLUSIVE_EXCLUSIVE);
-//        builder.setSpan(new ForegroundColorSpan(Color.RED), 41, 44, Spanned.SPAN_INCLUSIVE_EXCLUSIVE);
-//        textView.setText(builder);
-//        textView.setText("Hello World!\n\nHello World!\nHello World!\n\n\n\n\n");
-//        textView.setText("Hello World!\n\nHello World!\n\n\nHello World!\nHello World!\nHello World!");
     }
 }
